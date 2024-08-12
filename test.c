309ab0c42c61738f6dfaaca28aaab0a6184bb8c6
raffin <zerry0721@gmail.com>
2024-08-12 17:16:49 +0900

    test.c

---
+++ test.c
@@ -0,0 +1,52 @@
+// #include <stdio.h>
+
+// int main()
+// {  
+//   int i;
+//   int sum = 0;
+  
+//   for(i=1; i<=10; i=i+1)
+//   {
+//     printf("First:%d\n",sum);
+//     printf("indexI:%d\n",i);
+//     sum = sum+i;
+//     printf("sum+i:%d\n\n",sum);
+//   }
+  
+//   printf("%d", sum);
+
+//   return 0;
+// }
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int number;  
+// 	printf("정수를 입력하세요. : ");
+// 	scanf("%d\n", &number);
+//     printf("%d\n",number);
+// 	if (0 < number)
+// 	{
+// 		printf("양수입니다.");
+// 	}
+// 	else if (number < 0)
+// 	{
+// 		printf("음수입니다.");
+// 	}
+// 	else
+// 	{
+// 		printf("양수도 음수도 아닌 0입니다.");
+// 	}
+	
+// 	return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+	int number;  
+	printf("정수를 입력하세요. : ");
+	
+	return 0;
+}
